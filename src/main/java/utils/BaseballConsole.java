a3b5d08e4373b773ab54697b90169939c150ca43
Consome1 <78946348+Consome1@users.noreply.github.com>
2021-11-22 13:47:53 +0900

    feat: add BaseballConsole
    input 기능 추가

---
+++ src/main/java/utils/BaseballConsole.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class BaseballConsole {
+
+    public char[] input(int pIn) {
+        String strInput = Integer.toString(pIn);
+        char[] charInput = strInput.toCharArray();
+
+        return charInput;
+    }
+
+    public void print() {
+
+
+    }
+}
